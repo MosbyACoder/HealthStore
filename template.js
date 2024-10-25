fe5c5fcb2657c1ec92fdf0204fc2a4cb5550bd05
MosbyACoder <lfrase15@my.centennialcollege.ca>
2024-10-25 00:52:31 -0400

    update backend template

--- template.js
+++ template.js
@@ -3,10 +3,10 @@ export default () => {
     <html lang="en"> 
         <head>
             <meta charset="utf-8">
-            <title>Assignment 2</title> 
+            <title>Assignment 2 backend</title> 
         </head>
         <body>
-            <div id="root">Hello World</div> 
+            <div id="root">Backend - server(port 3000)</div> 
         </body>
     </html>`
 }
